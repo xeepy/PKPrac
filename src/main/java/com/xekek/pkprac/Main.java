2bc2aca11f97685cf056bd8998466f3f374fad66
xe <therazergamer60@gmail.com>
2025-06-21 07:06:15 -0600

    changed version

--- src/main/java/com/xekek/pkprac/Main.java
+++ src/main/java/com/xekek/pkprac/Main.java
@@ -48,7 +48,7 @@ import net.minecraftforge.fml.common.network.FMLNetworkEvent;
 public class Main
 {
     public static final String MODID = "PKPrac";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     private static int lastHurtTime = 0;
     public static boolean needsResync = false;
